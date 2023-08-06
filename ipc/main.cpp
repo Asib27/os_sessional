efd86ec1b91ef63edb57d0282c7a610ecc12f92b
asib27 <asib.rahman1927@gmail.com>
2023-08-07 00:35:09 +0600

    add submission station

--- ipc/main.cpp
+++ ipc/main.cpp
@@ -41,15 +41,21 @@ struct Student{
 pthread_mutex_t print_mutex;
 
 const int N_PS = 4; // number of print station
+const int N_STAFF = 2;
 int PS_TIME, BS_TIME, SS_TIME; // printing, binding, submission station time
 
 Timer timer; // for timing
 
 vector<Student*> students; 
+pthread_t staff[N_STAFF];
+int staff_id[N_STAFF];
+
 pthread_mutex_t ps_mutex[N_PS];
 bool ps_empty[N_PS];
 sem_t bs_sem;
 
+pthread_rwlock_t ss_rwlock;
+int n_submission;
 
 
 void *studentActivity(void *arg){
@@ -92,12 +98,41 @@ void *studentActivity(void *arg){
     pthread_mutex_unlock(&print_mutex);
 
     sleep(BS_TIME);
-    
+
     pthread_mutex_lock(&print_mutex);
     cout << "Group " << s.group << " has finished binding at time " << timer.get_time() << endl;
     pthread_mutex_unlock(&print_mutex);
 
     sem_post(&bs_sem);
+
+
+    // submission
+    pthread_rwlock_wrlock(&ss_rwlock);
+
+    n_submission++;
+    pthread_mutex_lock(&print_mutex);
+    cout << "Group " << s.group << " has submitted the report at time " << timer.get_time() << endl;
+    pthread_mutex_unlock(&print_mutex);
+
+    pthread_rwlock_unlock(&ss_rwlock);
+    return nullptr;
+}
+
+void *staff_activity(void * arg){
+    int staff = *(int *)arg + 1;
+    while(true){
+        int random = rand() % 10 + 3;
+        sleep(random);
+
+        pthread_rwlock_rdlock(&ss_rwlock);
+
+        pthread_mutex_lock(&print_mutex);
+        cout << "Staff " << staff << " has started reading the entry book at time "  
+            << timer.get_time() << ". No. of submission = " << n_submission << endl;
+        pthread_mutex_unlock(&print_mutex);
+
+        pthread_rwlock_unlock(&ss_rwlock);
+    }
     return nullptr;
 }
 
@@ -114,6 +149,7 @@ int main() {
     }
 
     sem_init(&bs_sem, 0, 2);
+    pthread_rwlock_init(&ss_rwlock, NULL);
 
     timer.start();
     for(int i = 0; i < N; i++){
@@ -123,6 +159,18 @@ int main() {
         pthread_create(&t->tid, NULL, studentActivity, (void *)t);
     }
 
-    pthread_exit(NULL);
+    for(int i = 0; i < N_STAFF; i++){
+        staff_id[i] = i;
+        pthread_create(&staff[i], NULL, staff_activity, &staff_id[i]);
+    }
+
+    for(auto i: students){
+        if(i->leader)
+            pthread_join(i->tid, NULL);
+    }
+    for(int i = 0; i < N_STAFF; i++){
+        pthread_cancel(staff[i]);
+    }
+    cout << "All thread finished" << endl;
     return 0;
 }
